ab7e003529ee483e029649039d7f7ac3d30ab42f
Doummar M.alzahabi <98951369+Doummarzhb@users.noreply.github.com>
2024-05-08 23:30:21 +0300

    smal

---
+++ Desktop/3alemnill/project with c#/abbyprojectrazor/AbbyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AbbyWeb.Model;
+
+namespace AbbyWeb.Data
+{
+	public class ApplicationDbContext
+	{
+		public IEnumerable<Category> Category { get; internal set; }
+
+		internal Task SaveChangesAsync()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
